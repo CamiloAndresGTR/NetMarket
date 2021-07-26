cd458c60242c32f34a140d344cf2c54569627de8
CamiloAndresGTR <76980247+CamiloAndresGTR@users.noreply.github.com>
2021-07-26 12:24:40 -0500

    26/07/2021 12:24 Implementación MiddleWare errores

---
+++ WebApi/Errors/CodeErrorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Errors
+{
+    public class CodeErrorException : CodeErrorResponse
+    {
+        public CodeErrorException(int statusCode, string message = null, string details = null) : base(statusCode, message)
+        {
+            Details = details;
+        }
+        public string Details { get; set; }
+    }
+}
