ca5be69e85b1620c27b85e9fc8e5f7675be60136
CamiloAndresGTR <76980247+CamiloAndresGTR@users.noreply.github.com>
2021-07-26 11:49:15 -0500

    26/07/2021 11:49 ErrorControles Implementado

---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/camilin/v1/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
---
+++ WebApi/Errors/CodeErrorResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Errors
+{
+    public class CodeErrorResponse
+    {
+        //Creo el constructor para poder definir errores personalizaods
+        public CodeErrorResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageStatusCode(statusCode);
+        }
+        // defino un método para consumir los errores por statusCode
+        private string GetDefaultMessageStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "El Request enviado tiene errores",
+                401 => "No tienes autorizacion para este recurso",
+                404 => "No se encontro el item buscado",
+                500 => "Se produjeron errores en el servidor",
+                _ => null
+            };
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
